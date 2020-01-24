5b1b1a03c6e13ead8b55fb23d6dddc0adb00c011
Daniel <daniel.jaramillo7@udea.edu.co>
2020-01-23 22:09:17 -0500

    Added code to read name from GET method

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
   <title>Prueba de PHP</title>
  </head>
  <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
+ <?php
+ if(isset($_GET['nombre'])) {
+    echo '¡Hola '.$_GET['nombre'].'!';
+ } else {
+    echo '¡Hola, persona de nombre desconocido!';
+ };
+ 
+ ?>
  </body>
 </html>
